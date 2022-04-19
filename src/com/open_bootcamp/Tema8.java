540f9381ad5e7bf458951da99fa97ce4b5ba2ac3
Oscar Rojas <rojasunalcol@gmail.com>
2022-04-18 21:28:34 -0500

    Curso introducción a la programación - Ejercicio Tema 8

--- src/com/open_bootcamp/Tema8.java
+++ src/com/open_bootcamp/Tema8.java
@@ -1,2 +1,46 @@
-package com.open_bootcamp;public class Tema8 {
+package com.open_bootcamp;
+
+public class Tema8 {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+
+        persona.setEdad(44);
+        persona.setNombre("Oscar Rojas");
+        persona.setTelefono("317382627");
+
+        System.out.println("Nombre = " + persona.getNombre());
+        System.out.println("Edad = " + persona.getEdad());
+        System.out.println("Telefono = " + persona.getTelefono());
+
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
